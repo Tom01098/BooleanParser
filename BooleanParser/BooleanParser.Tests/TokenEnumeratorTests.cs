ec3b526b4eed13a2c88cfb8b163a64e34e9f0fcc
Tom Humphreys <Tom01098Code@gmail.com>
2019-01-20 20:55:10 +0000

    Backtracking tests, UnexpectedTokenExceptions.

--- BooleanParser/BooleanParser.Tests/TokenEnumeratorTests.cs
+++ BooleanParser/BooleanParser.Tests/TokenEnumeratorTests.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BooleanParser.Tests
 {
@@ -48,6 +45,84 @@ namespace BooleanParser.Tests
             Assert.AreEqual(true, tokens.MoveNext());
             Assert.AreEqual(")", tokens.Current);
             Assert.AreEqual(false, tokens.MoveNext());
+        }
+
+        [TestMethod]
+        public void Backtrack()
+        {
+            var tokens = new TokenEnumerator("TRUE AND NOT FALSE");
+
+            Assert.AreEqual("TRUE", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            Assert.AreEqual("AND", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            tokens.SetBacktrackPoint();
+            Assert.AreEqual("NOT", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            tokens.Backtrack();
+            Assert.AreEqual("NOT", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            Assert.AreEqual("FALSE", tokens.Current);
+            Assert.AreEqual(false, tokens.MoveNext());
+        }
+
+        [TestMethod]
+        public void Backtrack2()
+        {
+            var tokens = new TokenEnumerator("(FALSE AND (NOT TRUE))");
+
+            tokens.SetBacktrackPoint();
+            Assert.AreEqual("(", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            Assert.AreEqual("FALSE", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            Assert.AreEqual("AND", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            tokens.SetBacktrackPoint();
+            Assert.AreEqual("(", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            Assert.AreEqual("NOT", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            Assert.AreEqual("TRUE", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            tokens.Backtrack();
+            Assert.AreEqual("(", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            Assert.AreEqual("NOT", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            Assert.AreEqual("TRUE", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            Assert.AreEqual(")", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            Assert.AreEqual(")", tokens.Current);
+            Assert.AreEqual(false, tokens.MoveNext());
+        }
+
+        [TestMethod]
+        public void UnexpectedToken()
+        {
+            var tokens = new TokenEnumerator("TRUE AND (TRUE OR X)");
+
+            Assert.AreEqual("TRUE", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            Assert.AreEqual("AND", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            tokens.SetBacktrackPoint();
+            Assert.AreEqual("(", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            Assert.AreEqual("TRUE", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+            Assert.AreEqual("OR", tokens.Current);
+            Assert.AreEqual(true, tokens.MoveNext());
+
+            try
+            {
+                throw tokens.UnexpectedToken();
+            }
+            catch (UnexpectedTokenException e)
+            {
+                Assert.AreEqual("X", e.Message);
+            }
         }
     }
 }
--- BooleanParser/BooleanParser/TokenEnumerator.cs
+++ BooleanParser/BooleanParser/TokenEnumerator.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace BooleanParser
 {
+    /// <summary>
+    /// Supports enumerating over tokens (words and parenthesis) while also
+    /// supporting stack-based backtracking.
+    /// </summary>
     public class TokenEnumerator
     {
         private readonly Stack<int> indexes = new Stack<int>();
@@ -22,16 +24,45 @@ namespace BooleanParser
             indexes.Push(0);
         }
 
+        /// <summary>
+        /// The current token
+        /// </summary>
         public string Current => tokens[indexes.Peek()];
 
+        /// <summary>
+        /// Push the current point onto the stack as a point that can be 
+        /// backtracked to using <see cref="Backtrack"/>.
+        /// </summary>
         public void SetBacktrackPoint() => indexes.Push(indexes.Peek());
 
+        /// <summary>
+        /// Go back to the appropriate point set using 
+        /// <see cref="SetBacktrackPoint"/>
+        /// </summary>
         public void Backtrack() => indexes.Pop();
 
+        /// <summary>
+        /// Move to the next token
+        /// </summary>
+        /// 
+        /// <returns>
+        /// Can <see cref="Current"/> be used?
+        /// </returns>
         public bool MoveNext()
         {
             indexes.Push(indexes.Pop() + 1);
             return tokens.Length > indexes.Peek();
         }
+
+        /// <summary>
+        /// Create a <see cref="UnexpectedTokenException"/> for the current
+        /// token.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// An <see cref="UnexpectedTokenException"/>.
+        /// </returns>
+        public UnexpectedTokenException UnexpectedToken() => 
+            new UnexpectedTokenException(tokens[indexes.Peek()]);
     }
 }
